7bbc514ad617277094df65921b650b3c537f0cb3
Tyler Klose <tylerklose@gmail.com>
2019-01-21 11:35:37 -0500

    add geocoder based on family home address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_022329) do
+ActiveRecord::Schema.define(version: 2019_01_21_162411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_01_15_022329) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "family_members", force: :cascade do |t|
